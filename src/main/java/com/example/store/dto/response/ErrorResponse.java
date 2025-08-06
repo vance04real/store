651367a0b09265adfcf84c0fbd1dfdb60b197bdc
evanskfchikuni <vance04real@gmail.com>
2025-08-06 22:58:38 +0200

    feature/TI-100/interview-task: made changes to the API spec , added ErrorResponse and refactor Controller Advice

---
+++ src/main/java/com/example/store/dto/response/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.example.store.dto.response;
+
+import java.time.Instant;
+
+public record ErrorResponse(String message,
+                            Instant timestamp,
+                            String path,
+                            int status) {
+    public static ErrorResponse of(String message, String path, int status) {
+        return new ErrorResponse(message, Instant.now(), path, status);
+    }
+}
